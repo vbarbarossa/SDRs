affbd65c8d5d642d972c2bb7a46358d7184216ae
vbarbarossa <vbarbarossa@science.ru.nl>
2020-01-21 12:36:03 +0100

    udt

--- R/model.R
+++ R/model.R
@@ -4,9 +4,9 @@ source('R/MASTER.R')
 #' load packages needed
 valerioUtils::libinv('dplyr')
 
-#' exclude stations with less than `r MIN_MONITORING_YEARS` years of monitoring
-#' From Gudmundsson et al., 2018:
+#' exclude stations with less than `r MIN_MONITORING_YEARS` years of monitoring.
 #' 
+#' Reliable yearly values are selected following Gudmundsson et al., 2018:
 #' *Index values at a yearly time step are reliable if at least 350 daily observations are declared reliable.*
 
 # use the homogeneity table to get a list of stations with MIN_MONITORING_YEARS or more good years of record
@@ -16,9 +16,28 @@ stations_selection <- read.csv(paste0(GSIM_dir,'GSIM_indices/HOMOGENEITY/yearly_
   select(gsim.no,ends_with('step'),ends_with('steps'))
 
 # scan through these stations and compute long-term mean of indices provided
+start_time <- Sys.time() #monitor time elapsed
 stations_indices <- lapply( # use lapply function that can be easily parallelized on linux
-  
-)
+  pull(stations_selection,gsim.no), # get the vector of names from the tibble
+  function(x){
+    tab <- read.csv(paste0(GSIM_dir,'GSIM_indices/TIMESERIES/yearly/',x,'.year'),skip = 21) %>% #skip first 21 rows which are not tabulated
+      dplyr::filter(n.available >= 350) %>%
+      select(-date,-n.missing,-n.available) %>%
+      #some files have been encoded as both csv and tab-delim
+      #therefore I need to clean the strings from extra '\t' separators and read them again as numeric
+      mutate_all(function(y) gsub('[\r\n\t]', '',y)) %>%
+      #some records have n.available = 365 but 0 values everywhere
+      #need to remove those. They have NA in CV (0/0) --> use that
+      filter(CV != 'NA') %>% # no need to use is.na() as at this stage they are still all characters
+      # and then convert to numeric to avoid warning messages
+      mutate_all(as.numeric)
+    
+    return(
+      bind_cols(data.frame(gsim.id = x),as.data.frame(t(colMeans(tab))) )
+    )
+  }
+) %>% do.call('rbind',.)
+Sys.time() - start_time
 
 #' exclude stations with an upstream catchment area < X km2
 #' 
