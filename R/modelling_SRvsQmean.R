389dfbcd4b112fbe699c5ea67c0ff253929ff5c3
Barbarossa <barbarossav@vuw.leidenuniv.nl>
2020-03-18 15:40:52 +0100

    udt

--- R/modelling_SRvsQmean.R
+++ R/modelling_SRvsQmean.R
@@ -261,6 +261,18 @@ d1$BAS <- as.factor(d1$BAS)
           panel.spacing = unit(2, "lines"))  # adding space between panels
 )
 
+#' multipanel
+#+ fig.width=10, fig.height=16
+ggplot(d1, aes(x = Q_MEAN, y = SR_tot, colour = BAS)) +
+  geom_point(alpha = 0.5) +
+  facet_wrap('BAS',ncol = 5) +
+  theme_bw() +
+  geom_line(data = cbind(d1, pred = predict(fit1)), aes(y = pred), size = 1, alpha = 0.3) +  # adding predicted line from mixed model
+  theme(legend.position = "none",
+        panel.grid = element_blank(),
+        panel.spacing = unit(2, "lines"))  # adding space between panels
+
+
 #' **Q2: is this last model (random slope and intercept) equivalent to fitting one lm per main basin?**
 
 # one lm for each main basin
